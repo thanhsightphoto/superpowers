db2eac8c2b10929858e4a83d70f777530751333c
thanhsightphoto <info_photography@thanhsight.com>
2026-06-27 13:29:31 -0700

    test(l5k-sim): lock nested/leading/series branch parsing

--- projects/l5k-logic-sim/tests/test_rung_parser.py
+++ projects/l5k-logic-sim/tests/test_rung_parser.py
@@ -1,7 +1,7 @@
 import pytest
 
 from l5k_sim.rung_parser import parse_rung
-from l5k_sim.ir import Instruction
+from l5k_sim.ir import Instruction, Branch
 
 
 def test_single_instruction():
@@ -40,3 +40,31 @@ def test_middle_instruction_operand_covered():
 def test_unexpected_character_raises():
     with pytest.raises(ValueError):
         parse_rung("@bad")
+
+
+def test_series_with_trailing_branch():
+    els = parse_rung("XIC(grace)[GT(cpm,max) OTE(over) ,LT(cpm,min) OTE(under) ]MOVE(Speed,RPM)")
+    assert isinstance(els[0], Instruction) and els[0].mnemonic == "XIC"
+    assert isinstance(els[1], Branch)
+    assert len(els[1].legs) == 2
+    assert [e.mnemonic for e in els[1].legs[0]] == ["GT", "OTE"]
+    assert [e.mnemonic for e in els[1].legs[1]] == ["LT", "OTE"]
+    assert isinstance(els[2], Instruction) and els[2].mnemonic == "MOVE"
+
+
+def test_leading_branch_then_branch():
+    text = "[XIC(a) ,XIO(b) ONS(ons.1) ][CPT(x,1) ,RES(c) RES(d) ]"
+    els = parse_rung(text)
+    assert isinstance(els[0], Branch) and isinstance(els[1], Branch)
+    assert [e.mnemonic for e in els[0].legs[1]] == ["XIO", "ONS"]
+    assert [e.mnemonic for e in els[1].legs[1]] == ["RES", "RES"]
+
+
+def test_nested_branch():
+    text = "XIC(en)[GT(a,b) ,[LT(c,d) ,EQ(e,f) ]OTE(g) ]"
+    els = parse_rung(text)
+    branch = els[1]
+    assert isinstance(branch, Branch)
+    inner = branch.legs[1][0]
+    assert isinstance(inner, Branch)
+    assert [leg[0].mnemonic for leg in inner.legs] == ["LT", "EQ"]
